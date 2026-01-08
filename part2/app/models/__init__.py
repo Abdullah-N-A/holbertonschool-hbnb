ea0878c65cd352728365d8f3dab3722e509e7ec9
Abdullah-N-A <Abdullahttlrx@gmail.com>
2026-01-08 20:00:52 +0300

    new up

--- part2/app/models/__init__.py
+++ part2/app/models/__init__.py
@@ -4,11 +4,12 @@ Models package initializer.
 This module provides access to the core business logic models.
 """
 
-from models.base_model import BaseModel
-from models.user import User
-from models.place import Place
-from models.review import Review
-from models.amenity import Amenity
+from .base_model import BaseModel
+from .user import User
+from .place import Place
+from .review import Review
+from .amenity import Amenity
+
 
 __all__ = [
     "BaseModel",
