5ad18f0a3a4e4bd84fda44f14f2baae2229bf937
Abdullah-N-A <Abdullahttlrx@gmail.com>
2026-01-31 16:50:41 +0300

    add config.py file

---
+++ part3/config.py
@@ -0,0 +1,20 @@
+import os
+
+class Config:
+    SECRET_KEY = os.getenv("SECRET_KEY", "default_secret_key")
+    DEBUG = False
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = "sqlite:///development.db"
+
+class TestingConfig(Config):
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = "sqlite:///:memory:"
+
+config = {
+    "development": DevelopmentConfig,
+    "testing": TestingConfig,
+    "default": DevelopmentConfig,
+}
