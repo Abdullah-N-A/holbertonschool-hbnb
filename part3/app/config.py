97d8b1c57e2a5c4808dd3ee325dd5d173640591f
Abdullah-N-A <Abdullahttlrx@gmail.com>
2026-01-31 16:57:10 +0300

    add extnsion.py

--- part3/app/config.py
+++
@@ -1,7 +0,0 @@
-# app/config.py
-import os
-
-class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY', 'supersecretkey')
-    JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY', 'jwt-secret-string')
-    JWT_ACCESS_TOKEN_EXPIRES = 3600  # Optional: 1 hour token expiration
